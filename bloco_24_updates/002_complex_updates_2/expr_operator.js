74344cd092b000d7c1431424102c22c665db3474
Aladino Borges <74330016+AladinoBorges@users.noreply.github.com>
2021-08-16 14:32:16 -0300

    limpeza do texto do último commit.

--- bloco_24_updates/002_complex_updates_2/expr_operator.js
+++ bloco_24_updates/002_complex_updates_2/expr_operator.js
@@ -15,4 +15,4 @@ db.monthlyBudget.insertMany(DATA);
 
 db.monthlyBudget.find({ $expr: { $gt: FILTERS } }); //* esta query utiliza o operador $expr para buscar os documentos em que o valor de spent exceda o valor de budget.
 
-//! Note que, na query , nenhum valor foi especificado explicitamente. O que acontece é que o operador $expr entende que deve comparar os valores dos dois campos. Por isso o $ é utilizado, indicando que a string entre aspas referencia um campo. Mais à frente no curso, você verá muito mais aplicações para o operador $expr dentro do tópico sobre Aggregation Framework , e verá que suas queries ficarão muito mais poderosas!
+//! Note que, na query, nenhum valor foi especificado explicitamente. O que acontece é que o operador $expr entende que deve comparar os valores dos dois campos. Por isso o $ é utilizado, indicando que a string entre aspas referencia um campo. Mais à frente no curso, você verá muito mais aplicações para o operador $expr dentro do tópico sobre Aggregation Framework, e verá que suas queries ficarão muito mais poderosas!
