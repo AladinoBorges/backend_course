edb4e5088e4e2af674b3bb0ee46a3620607e8f5d
Aladino Borges <74330016+AladinoBorges@users.noreply.github.com>
2021-08-24 22:29:16 -0300

    Bloco 26.1: Introdução NodeJS.

---
+++ bloco_26_node_js/001_introduction/simple_scripts/index.js
@@ -0,0 +1,7 @@
+const { question, questionInt } = require("readline-sync");
+
+const name = question("Escreva o seu nome: ");
+const age = questionInt("Insira o seu nome: ");
+
+console.log(`Hello, ${name}! Tu tens ${age} anos de idade.`);
+// console.log("Hello, mundo!");
